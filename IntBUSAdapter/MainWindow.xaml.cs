fa5aaa3795f270cd34b15db64f7e1f3cd9db1eca
mokap13 <mokap13@mail.ru>
2018-02-28 21:13:06 +0400

    Добавил преамбулу

--- IntBUSAdapter/MainWindow.xaml.cs
+++ IntBUSAdapter/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Globalization;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Threading;
 
 namespace IntBUSAdapter
 {
@@ -36,6 +37,7 @@ namespace IntBUSAdapter
 
         #endregion
     }
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -43,6 +45,7 @@ namespace IntBUSAdapter
     {
         private int expectByteCount;
 
+        public List<byte> Preambula { get; set; }
         public SerialPort SerialPortModbus { get; set; }
         public SerialPort SerialPortIntBUS { get; set; }
         public ObservableCollection<string> Parities { get; set; }
@@ -59,8 +62,10 @@ namespace IntBUSAdapter
             SerialPortModbus = new SerialPort();
             SerialPortModbus.DataReceived += new SerialDataReceivedEventHandler(SerialDataModbusReceived);
             SerialPortIntBUS = new SerialPort();
+            //SerialPortIntBUS.ReadTimeout = 5;
             SerialPortIntBUS.DataReceived += new SerialDataReceivedEventHandler(SerialDataIntBUSResponse);
             IntBusAddedData = new List<byte>();
+            Preambula = new List<byte>();
             SerialPortNames = new ObservableCollection<string>();
             Parities = new ObservableCollection<string>();
             StopBites = new ObservableCollection<string>();
@@ -70,8 +75,7 @@ namespace IntBUSAdapter
                 50, 1200, 2400, 4800, 9600, 19200, 38400,
                 56000, 57600, 76800, 115200, 230400, 460800
             };
-            expectByteCount = 7;
-            IntBusAddedData.Add(0x21);
+            expectByteCount = 1;
             foreach (string name in SerialPort.GetPortNames())
                 SerialPortNames.Add(name);
 
@@ -105,7 +109,8 @@ namespace IntBUSAdapter
                 listBytes.AddRange(ModbusUtility.CalculateCrc(listBytes.ToArray()));
             }
 
-           
+            listBytes.InsertRange(0, Preambula);
+
             if (SerialPortIntBUS != null)
                 if (SerialPortIntBUS.IsOpen)
                 {
@@ -119,7 +124,7 @@ namespace IntBUSAdapter
                     AddConsoleText(message + "\r", textBox_ConsoleIntBUS, Brushes.NavajoWhite);
                 }
         }
-   
+
         private string AddTime(string str)
         {
             return str.Insert(0, DateTime.Now.ToString("HH:mm:ss.fff") + "    ");
@@ -128,7 +133,7 @@ namespace IntBUSAdapter
         {
             SerialPort serialPort = (SerialPort)sender;
             int byteRecieved = serialPort.BytesToRead;
-            byte[] messByte = new byte[byteRecieved];
+            byte[] messByte = new byte[byteRecieved]; ;
             if (messByte.Length < expectByteCount)
                 return;
             serialPort.Read(messByte, 0, byteRecieved);
@@ -137,7 +142,7 @@ namespace IntBUSAdapter
             message = AddTime(message);
 
             if (SerialPortModbus != null)
-                if(SerialPortModbus.IsOpen)
+                if (SerialPortModbus.IsOpen)
                 {
                     SerialPortModbus.Write(messByte, 0, messByte.Length);
                     AddConsoleText(message + "\r", textBox_ConsoleModbus, Brushes.LimeGreen);
@@ -193,7 +198,7 @@ namespace IntBUSAdapter
         private void ToggleButton_OpenOutPort_Checked(object sender, RoutedEventArgs e)
         {
             ToggleButton button = (sender as ToggleButton);
-            
+
             try
             {
                 if (!SerialPortIntBUS.IsOpen)
@@ -375,7 +380,24 @@ namespace IntBUSAdapter
             }
 
         }
+        private void Preambula_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                string[] data = (sender as TextBox).Text.Split(' ');
+                Preambula.Clear();
+                foreach (string strByte in data)
+                {
+                    Preambula.Add(byte.Parse(strByte, NumberStyles.HexNumber));
+                }
+                Preambula.Reverse();
+            }
+            catch (Exception)
+            {
 
+
+            }
+        }
         #endregion
 
         public static string ByteArrayToString(byte[] ba)
